eaebffaddbf9d334b6b12636d9b2df0f846dd72c
Kaj Jansson <mailtokaj@gmail.com>
2024-10-30 14:20:15 +0100

    Redesigned ColorKey and moved plotting functions to ReColor

--- src/bin/rgb_demo.rs
+++ src/bin/rgb_demo.rs
@@ -2,20 +2,18 @@ use std::ops::Range;
 
 use net_neurons::{
     nnetwork::FloatType,
-    recolor::{
-        color_key::{ColorKey, RGB_VENN_DIAGRAM},
-        RGBPredictor,
-    },
-};
-use plotters::{
-    chart::{ChartBuilder, LabelAreaPosition},
-    prelude::{BitMapBackend, Cross, IntoDrawingArea, Rectangle},
-    style::{RGBAColor, ShapeStyle, RED, WHITE},
+    recolor::ReColor,
 };
 
 fn main() {
-    // Specifies wheter a pixel is red, green and/or blue
-    const COLOR_KEY: ColorKey = RGB_VENN_DIAGRAM;
+    // Example "color key" representing three overlapping spheres.
+    let rgb_venn_diagram = &|(x, y): (FloatType, FloatType)| {
+        [
+            (x - 0.2165).powi(2) + (y + 0.125).powi(2) < 0.25,
+            (x + 0.2165).powi(2) + (y + 0.125).powi(2) < 0.25,
+            x.powi(2) + (y - 0.25).powi(2) < 0.25,
+        ]
+    };
     // Every neural network will have an input and output layer. The hidden layers are inbetween these.
     const N_HIDDEN_LAYERS: usize = 3;
     // The layers are square so the number of neurons is this number squared
@@ -24,11 +22,16 @@ fn main() {
     const REGULARIZATION: Option<FloatType> = None;
 
     // Instantiate the network
-    let mut categorize = RGBPredictor::new(COLOR_KEY, N_HIDDEN_LAYERS, LAYER_SIZE, REGULARIZATION);
+    let mut recolor = ReColor::new(
+        rgb_venn_diagram,
+        N_HIDDEN_LAYERS,
+        LAYER_SIZE,
+        REGULARIZATION,
+    );
 
     // Import previously exported parameters if able. Will fallback to random initiated neurons if the file does not exist, but panic on other errors.
     const IMPORT_FILENAME: &str = "rgb.param";
-    match categorize.import_parameters(IMPORT_FILENAME) {
+    match recolor.import_parameters(IMPORT_FILENAME) {
         Ok(_) => println!("Successful parameter import from {IMPORT_FILENAME}."),
         Err(err) => match err.kind() {
             std::io::ErrorKind::NotFound | std::io::ErrorKind::UnexpectedEof => {
@@ -42,7 +45,7 @@ fn main() {
     }
 
     // The number of updates to the netowrk
-    const TRAINING_CYCLES: usize = 100000;
+    const TRAINING_CYCLES: usize = 10000;
     // The number of data points to use before updating the network by back propagation
     const TRAINING_BATCH_SIZE: usize = 100;
     // The range of learning rates to be used. Will be logspaced.
@@ -53,7 +56,7 @@ fn main() {
     // Lots of text...or not
     const VERBOSE: bool = true;
     // Returns a vector of learning rates and loss values
-    let training_data = categorize.train(
+    recolor.train(
         TRAINING_CYCLES,
         TRAINING_BATCH_SIZE,
         LEARNING_RATE,
@@ -64,117 +67,15 @@ fn main() {
 
     // Save the resulting network. Will overwrite any existing file!
     const EXPORT_FILENAME: &str = "rgb.param";
-    if let Ok(filename) = categorize.export_parameters(EXPORT_FILENAME) {
+    if let Ok(filename) = recolor.export_parameters(EXPORT_FILENAME) {
         println!("Exported RGB parameters to {filename}");
     }
 
     // Plots the colours predicted by the network for a sample of coordinates
     const X_SAMPLES: u32 = 500;
-    plot_predictions(&categorize, &X_RANGE, &Y_RANGE, X_SAMPLES).unwrap();
+    const PLOT_PREDICTION_FILENAME: &str = "plot_rgb.png";
+    recolor.plot_predictions(&X_RANGE, &Y_RANGE, X_SAMPLES, PLOT_PREDICTION_FILENAME).unwrap();
     // Plots a diagram of log(loss) vs p(learning rate)
-    plot_training_progress(&training_data).unwrap();
-}
-
-fn plot_predictions(
-    predictor: &RGBPredictor,
-    x_range: &Range<FloatType>,
-    y_range: &Range<FloatType>,
-    x_divisions: u32,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let size = (x_range.end - x_range.start, y_range.end - y_range.start);
-    let division = (
-        x_divisions,
-        (x_divisions as FloatType * size.1 / size.0) as usize,
-    );
-    let step = (
-        size.0 / division.0 as FloatType,
-        size.1 / division.1 as FloatType,
-    );
-
-    const X_PIXELS: u32 = 1000;
-    let drawing_area = BitMapBackend::new(
-        "plot_rgb.png",
-        (X_PIXELS, (X_PIXELS as FloatType * size.1 / size.0) as u32),
-    )
-    .into_drawing_area();
-    drawing_area.fill(&WHITE)?;
-    let mut chart = ChartBuilder::on(&drawing_area)
-        .x_label_area_size(0)
-        .y_label_area_size(0)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .build_cartesian_2d(x_range.start..x_range.end, y_range.start..y_range.end)?;
-
-    chart.draw_series((0..division.0).flat_map(|xi| {
-        let xl = x_range.start + step.0 * xi as FloatType;
-        let xr = xl + step.0;
-        let xm = xl + step.0 * 0.5;
-        (0..division.1).map(move |yi| {
-            let yt = y_range.start + step.1 * yi as FloatType;
-            let yb = yt + step.1;
-            let ym = yt + step.1 * 0.5;
-            let rgb: Vec<_> = predictor
-                .predict((xm, ym))
-                .iter()
-                .map(|c| (c * 255.0) as u8)
-                .collect();
-            let color = RGBAColor(rgb[0], rgb[1], rgb[2], 1.);
-            Rectangle::new(
-                [(xl, yt), (xr, yb)],
-                ShapeStyle {
-                    color,
-                    filled: true,
-                    stroke_width: 0,
-                },
-            )
-        })
-    }))?;
-    chart.configure_mesh().disable_mesh().draw()?;
-
-    drawing_area.present()?;
-    Ok(())
-}
-
-fn plot_training_progress(
-    training_data: &[(FloatType, FloatType)],
-) -> Result<(), Box<dyn std::error::Error>> {
-    const X_PIXELS: u32 = 1024;
-    const Y_PIXELS: u32 = 768;
-    let drawing_area =
-        BitMapBackend::new("plot_loss.png", (X_PIXELS, Y_PIXELS)).into_drawing_area();
-    let min = training_data
-        .iter()
-        .fold((FloatType::MAX, FloatType::MAX), |acc, (x, y)| {
-            (x.min(acc.0), y.min(acc.1))
-        });
-    let max = training_data
-        .iter()
-        .fold((FloatType::MIN, FloatType::MIN), |acc, (x, y)| {
-            (x.max(acc.0), y.max(acc.1))
-        });
-    let x_begin = -max.0.log10();
-    let x_end = -min.0.log10();
-    let y_begin = min.1.log10();
-    let y_end = max.1.log10();
-    drawing_area.fill(&WHITE)?;
-    let mut chart = ChartBuilder::on(&drawing_area)
-        .x_label_area_size(0)
-        .y_label_area_size(0)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .build_cartesian_2d(x_begin..x_end, y_begin..y_end)?;
-
-    chart.draw_series(
-        training_data.iter().map(|(learning_rate, loss)| {
-            Cross::new((-learning_rate.log10(), loss.log10()), 5, RED)
-        }),
-    )?;
-    chart
-        .configure_mesh()
-        .x_desc("Neg log10 learning rate")
-        .y_desc("Log10 Loss")
-        .draw()?;
-
-    drawing_area.present()?;
-    Ok(())
+    const PLOT_PROGRESS_FILENAME: &str = "plot_loss.png";
+    recolor.plot_training_progress(PLOT_PROGRESS_FILENAME).unwrap();
 }
--- src/recolor/rgb_predictor.rs
+++ src/recolor/rgb_predictor.rs
@@ -1,21 +1,31 @@
+use plotters::{
+    chart::{ChartBuilder, LabelAreaPosition},
+    prelude::{BitMapBackend, Cross, IntoDrawingArea, Rectangle},
+    style::{RGBAColor, ShapeStyle, RED, WHITE},
+};
 use rand_distr::{Distribution, Uniform};
-use std::{ops::Range, time::Instant};
+use std::{error::Error, fmt::Display, ops::Range, time::Instant};
 
 use crate::nnetwork::{
     loss_functions::least_squares, CalcNode, FloatType, FunctionLayer, Layer, LinearLayer,
     MultiLayer, Parameters,
 };
 
-use crate::recolor::color_key::ColorKey;
-
 /// Manages the construction and training of a network that decides what color a pixel should have.
-pub struct RGBPredictor {
-    _color_key: ColorKey,
+pub struct ReColor<T>
+where
+    T: Fn((FloatType, FloatType)) -> [bool; 3],
+{
+    _color_key: T,
     _mlp: MultiLayer,
     _regularization: Option<FloatType>,
+    _training_results: Vec<(FloatType,FloatType)>,
 }
 
-impl RGBPredictor {
+impl<T> ReColor<T>
+where
+    T: Fn((FloatType, FloatType)) -> [bool; 3],
+{
     // Helps ctor
     fn create_layers(n_hidden_layers: usize, layer_size: usize) -> Vec<Box<dyn Layer>> {
         const BIASED_LAYERS: bool = true;
@@ -46,7 +56,7 @@ impl RGBPredictor {
             )));
             layers.push(Box::new(non_linearity.clone()));
         }
-        
+
         // Output layer
         layers.push(Box::new(LinearLayer::new_rand(
             OUTPUT_DIM,
@@ -61,18 +71,19 @@ impl RGBPredictor {
 
     /// After each linear layer a non-linear [FunctionLayer] is inserted.
     pub fn new(
-        color_key: ColorKey,
+        color_key: T,
         n_hidden_layers: usize,
         layer_size: usize,
         regularization: Option<FloatType>,
-    ) -> RGBPredictor {
+    ) -> ReColor<T> {
         let mut mlp = MultiLayer::new(Self::create_layers(n_hidden_layers, layer_size));
         mlp.set_regularization(regularization);
         mlp.set_loss_function(&least_squares);
-        RGBPredictor {
+        ReColor {
             _color_key: color_key,
             _mlp: mlp,
             _regularization: regularization,
+            _training_results: Vec::new(),
         }
     }
 
@@ -119,7 +130,7 @@ impl RGBPredictor {
 
     /// Trains the network for the specified number of cycles. Each cycles uses ´batch_size´ data points.
     /// The learning rate is a range from highest to lowest which will be logspaced so that the learning rate get lower for each cycle.
-    /// 
+    ///
     /// Returns a vector of learning rates and loss values
     pub fn train(
         &mut self,
@@ -129,9 +140,9 @@ impl RGBPredictor {
         x_range: &Range<FloatType>,
         y_range: &Range<FloatType>,
         verbose: bool,
-    ) -> Vec<(FloatType, FloatType)> {
+    ) -> &[(FloatType, FloatType)] {
         let timer = Instant::now();
-        let mut training_points = Vec::new();
+        self._training_results.clear();
         let mut loss = 0.;
         let learning_rate_log_step = if cycles < 2 {
             learning_rate.start
@@ -145,7 +156,7 @@ impl RGBPredictor {
                 (learning_rate.start.ln() + learning_rate_log_step * n as FloatType).exp();
             loss = self._mlp.train(&correlations, learning_rate);
 
-            training_points.push((learning_rate, loss));
+            self._training_results.push((learning_rate, loss));
 
             // Provide some per cycle stats
             if verbose {
@@ -162,7 +173,7 @@ impl RGBPredictor {
             self._mlp.param_iter().map(|p| p.len()).sum::<usize>(),
             timer.elapsed().as_millis(), loss
         );
-        training_points
+        &self._training_results
     }
 
     pub fn export_parameters(&self, filename: &str) -> std::io::Result<String> {
@@ -172,4 +183,121 @@ impl RGBPredictor {
     pub fn import_parameters(&mut self, filename: &str) -> Result<(), std::io::Error> {
         self._mlp.import_parameters(filename)
     }
+
+    /// Plots the colours predicted by the network for a sample of coordinates.
+    pub fn plot_predictions(
+        &self,
+        x_range: &Range<FloatType>,
+        y_range: &Range<FloatType>,
+        x_divisions: u32,
+        filename: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let size = (x_range.end - x_range.start, y_range.end - y_range.start);
+        let division = (
+            x_divisions,
+            (x_divisions as FloatType * size.1 / size.0) as usize,
+        );
+        let step = (
+            size.0 / division.0 as FloatType,
+            size.1 / division.1 as FloatType,
+        );
+
+        const X_PIXELS: u32 = 1000;
+        let drawing_area = BitMapBackend::new(
+            filename,
+            (X_PIXELS, (X_PIXELS as FloatType * size.1 / size.0) as u32),
+        )
+        .into_drawing_area();
+        drawing_area.fill(&WHITE)?;
+        let mut chart = ChartBuilder::on(&drawing_area)
+            .x_label_area_size(0)
+            .y_label_area_size(0)
+            .set_label_area_size(LabelAreaPosition::Left, 40)
+            .set_label_area_size(LabelAreaPosition::Bottom, 40)
+            .build_cartesian_2d(x_range.start..x_range.end, y_range.start..y_range.end)?;
+
+        chart.draw_series((0..division.0).flat_map(|xi| {
+            let xl = x_range.start + step.0 * xi as FloatType;
+            let xr = xl + step.0;
+            let xm = xl + step.0 * 0.5;
+            (0..division.1).map(move |yi| {
+                let yt = y_range.start + step.1 * yi as FloatType;
+                let yb = yt + step.1;
+                let ym = yt + step.1 * 0.5;
+                let rgb: Vec<_> = self
+                    .predict((xm, ym))
+                    .iter()
+                    .map(|c| (c * 255.0) as u8)
+                    .collect();
+                let color = RGBAColor(rgb[0], rgb[1], rgb[2], 1.);
+                Rectangle::new(
+                    [(xl, yt), (xr, yb)],
+                    ShapeStyle {
+                        color,
+                        filled: true,
+                        stroke_width: 0,
+                    },
+                )
+            })
+        }))?;
+        chart.configure_mesh().disable_mesh().draw()?;
+
+        drawing_area.present()?;
+        Ok(())
+    }
+    
+    /// Plots a diagram of log(loss) vs p(learning rate).
+    pub fn plot_training_progress(
+        &self,
+        filename: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        if self._training_results.is_empty(){
+            return Err(Box::new(TrainingError{}));
+        }
+        const X_PIXELS: u32 = 1024;
+        const Y_PIXELS: u32 = 768;
+        let drawing_area = BitMapBackend::new(filename, (X_PIXELS, Y_PIXELS)).into_drawing_area();
+        let min = self._training_results
+            .iter()
+            .fold((FloatType::MAX, FloatType::MAX), |acc, (x, y)| {
+                (x.min(acc.0), y.min(acc.1))
+            });
+        let max = self._training_results
+            .iter()
+            .fold((FloatType::MIN, FloatType::MIN), |acc, (x, y)| {
+                (x.max(acc.0), y.max(acc.1))
+            });
+        let x_begin = -max.0.log10();
+        let x_end = -min.0.log10();
+        let y_begin = min.1.log10();
+        let y_end = max.1.log10();
+        drawing_area.fill(&WHITE)?;
+        let mut chart = ChartBuilder::on(&drawing_area)
+            .x_label_area_size(0)
+            .y_label_area_size(0)
+            .set_label_area_size(LabelAreaPosition::Left, 40)
+            .set_label_area_size(LabelAreaPosition::Bottom, 40)
+            .build_cartesian_2d(x_begin..x_end, y_begin..y_end)?;
+
+        chart.draw_series(self._training_results.iter().map(|(learning_rate, loss)| {
+            Cross::new((-learning_rate.log10(), loss.log10()), 5, RED)
+        }))?;
+        chart
+            .configure_mesh()
+            .x_desc("Neg log10 learning rate")
+            .y_desc("Log10 Loss")
+            .draw()?;
+
+        drawing_area.present()?;
+        Ok(())
+    }
 }
+
+#[derive(Debug)]
+struct TrainingError{}
+impl Display for TrainingError{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f,"No training data available.")
+    }
+}
+impl Error for TrainingError{}
